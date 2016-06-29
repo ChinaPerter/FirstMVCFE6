d513cedc3c84b437032840f39b915c067bb8beb2
ChinaPerter <zjp_sunday@126.net>
2016-06-29 10:21:16 +0800

    安装EntityframeWork

---
+++ FirstMVCFE6/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstMVCFE6.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.LoginState = "登录前。。。。。";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(FormCollection fc   )
+        {
+            string strEmail = fc["InputEmail"];
+            string password = fc["InputPassword"];
+
+            ViewBag.LoginState = strEmail +   "登录后。。。。。";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        //public ActionResult Action()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public ActionResult Action()
+        //{
+        //    return View();
+        //}
+
+	}
+}
